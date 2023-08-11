90181a33a386f3bf6e8085fc5ca593169afbc17c
olaitan01 <abiodunfatunbi4@gmail.com>
2023-08-11 13:58:38 -0700

    loop

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * mian - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i < 11)
+	{
+		printf("%d\n",	i++);
+	}
+		return (0);
+}
