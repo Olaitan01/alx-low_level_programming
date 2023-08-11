34d67da19d57a8fe138a56d4238411a75e0cc1f8
olaitan01 <abiodunfatunbi4@gmail.com>
2023-08-11 15:22:48 -0700

    print hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	for (int num = '0'; num <= '9'; num++)
+	{
+		putchar(num);
+	}
+
+	for (char alpha = 'a'; alpha < 'g'; alpha++)
+	{
+		putchar(alpha);
+	}
+		putchar('\n');
+		return (0);
+}
