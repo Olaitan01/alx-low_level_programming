ff8495ee84968e630e02539c61c45e8d65c30abe
olaitan01 <abiodunfatunbi4@gmail.com>
2023-08-15 11:05:00 -0700

    Print lowercase 10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Prints alphabet x10
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	char a;
+	int b;
+	b = 0;
+	
+	while (b < 10){
+		for (a = 'a'; a <= 'z'; a++)
+		{
+			_putchar(a);
+		}
+		_putchar('\n');
+		b++;
+	}
+}
