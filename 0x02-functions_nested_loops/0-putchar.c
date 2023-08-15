962b7b96720c694b1478781c9388ad31d2453c1e
olaitan01 <abiodunfatunbi4@gmail.com>
2023-08-15 09:42:28 -0700

    Print _putchar using putchar()

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char a = '_';
+	char b = 'p';
+	char c = 'u';
+	char d = 't';
+	char e = 'c';
+	char f = 'h';
+	char g = 'a';
+	char h = 'r';
+
+	putchar(a);
+	putchar(b);
+	putchar(c);
+	putchar(d);
+	putchar(e);
+	putchar(f);
+	putchar(g);
+	putchar(h);
+	putchar('\n');
+
+	return (0);
+}
