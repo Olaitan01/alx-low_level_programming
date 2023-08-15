c59a3b0e3c209398fa8ce44f9030befeceaff556
olaitan01 <abiodunfatunbi4@gmail.com>
2023-08-15 10:02:17 -0700

    print lowercase with printchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+
+		for (a = 'a'; a <= 'z'; a++)
+		{
+			putchar(a);
+		}
+	putchar('\n');
+	return (0);
+}
