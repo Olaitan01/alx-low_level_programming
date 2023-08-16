4ad2dd51f4fd93364684c164af9c94df8ebc5a3c
olaitan01 <abiodunfatunbi4@gmail.com>
2023-08-16 03:54:29 -0700

    fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+int main(void)
+{
+	long int i;
+	long int j;
+	long int k;
+	long int next;
+
+	j = 1;
+	k = 2;
+
+	for (i = 1; i <= 50; i++)
+	{
+		if (j != 20365011074)
+		{
+			printf("%ld, ", j);
+		}
+		else
+		{
+			printf("%ld\n", j);
+		}
+		next = j + k;
+		j = k;
+		k = next;
+	}
+	return (0);
+}
