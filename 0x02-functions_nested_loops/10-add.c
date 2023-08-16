34dd4901f422e2ba2102d1393f9e2c30b3d84422
olaitan01 <abiodunfatunbi4@gmail.com>
2023-08-16 01:17:53 -0700

    print sum

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - returns the sum of the parameters
+ *
+ * @c: The value to be passed as parameter
+ * @b: The value to be passed as parameter
+ * Return: Always 0
+ */
+int add(int c, int b)
+{
+	int result = c + b;
+
+	return (result);
+}
