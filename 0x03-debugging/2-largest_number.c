a76615a7f0aa522cf1fd4d1cce7363d7a2d06893
olaitan01 <abiodunfatunbi4@gmail.com>
2023-08-17 07:07:03 -0700

    hot fix

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,11 +12,11 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a >= b && c >= b)
+	if (a >= b && a >= c)
 	{
 		largest = a;
 	}
-	else if (c >= b && a >= c)
+	else if (c >= b && c >= a)
 	{
 		largest = b;
 	}
