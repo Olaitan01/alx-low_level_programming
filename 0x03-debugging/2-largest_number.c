7363ac04db12a2bdc5f9fa74d57384f27ac2b9f0
olaitan01 <abiodunfatunbi4@gmail.com>
2023-08-17 06:55:24 -0700

    hot fix

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,10 +1,10 @@
 #include "main.h"
-
+#include <stdio.h>
 
 /**
  * largest_number - returns the largest of 3 numbers
  * @a: first integer
- * @b: sexond integer
+ * @b: second integer
  * @c: third integer
  * Return: largest number
  */
