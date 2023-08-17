6a1e3f956c2d409f9704bc4e2ada24b682cff9de
olaitan01 <abiodunfatunbi4@gmail.com>
2023-08-17 03:32:16 -0700

    first push

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - returns a negative or  positive
+ * if integer is either negative or positive
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 98;
+	positive_or_negative(i);
+
+	return (0);
+}
