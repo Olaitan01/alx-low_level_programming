78211d62136ce12149b610b10edd34e8de848042
olaitan01 <abiodunfatunbi4@gmail.com>
2023-08-24 11:53:49 -0700

    first push

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -17,14 +17,16 @@ int _strcmp(char *s1, char *s2)
 		{
 			return (*s1 - *s2);
 		}
-		else if (*s2 != *s1)
-		{
-			return (*s2 - *s1);
-		}
-		else
-		{
-			return (*s1 - *s1);
-		}
+		/**	
+		*else if (*s2 != *s1)
+		*{
+		*	return (*s2 - *s1);
+		*}
+		*else
+		*{
+		*	return (*s1 - *s1);
+		*}
+		*/
 		s1++;
 		s2++;
 	}
